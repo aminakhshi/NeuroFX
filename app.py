624289650ef132f532939e4acad72970cbdd59c0
amin <amin.akhshi@gmail.com>
2023-09-19 10:42:58 -0400

    initial release

--- app.py
+++ app.py
@@ -1,32 +1,81 @@
 import os
-
+import sys
 from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+                   send_from_directory, url_for, session, render_template_string)
+import plotly.express as px
+import plotly.io as pio
+import subprocess
 
 app = Flask(__name__)
-
+app.secret_key = "some_secret_key"
 
 @app.route('/')
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/start_game', methods=['POST'])
+def start_game():
+    python_path = sys.executable
+    # Run the game script externally
+    result = subprocess.run(
+        [python_path, os.path.join(app.root_path, 'game_stimuli.py')],
+        capture_output=True, text=True
+    )
+
+    # Print the output for debugging
+    print("STDOUT:", result.stdout)
+    print("STDERR:", result.stderr)
+
+    # Fetch the results (assuming they are stored in a file)
+    with open("game_results.txt", "r") as f:
+        results = eval(f.read())  # Using eval with caution
+
+    session['results'] = results
+    return redirect(url_for('show_results'))
+
+@app.route('/show_results')
+def show_results():
+    results = session.get('results', {})
+
+    # Generate plots
+    fig1 = px.bar(x=['Correct', 'Wrong'], y=[results.get('correct_clicks', 0), results.get('incorrect_clicks', 0)], title="Correct vs Wrong Answers")
+    fig2 = px.histogram(results.get('correct_latencies', []), nbins=20, title="Latency Times for Correct Answers")
+    fig3 = px.histogram(results.get('incorrect_latencies', []), nbins=20, title="Latency Times for Incorrect Answers")
+
+    bar_html = pio.to_html(fig1, full_html=False)
+    correct_hist_html = pio.to_html(fig2, full_html=False)
+    incorrect_hist_html = pio.to_html(fig3, full_html=False)
+
+    return render_template_string("""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Game Analysis Results</title>
+    </head>
+    <body>
+        <div>{{ bar_html|safe }}</div>
+        <div>{{ correct_hist_html|safe }}</div>
+        <div>{{ incorrect_hist_html|safe }}</div>
+    </body>
+    </html>
+    """, bar_html=bar_html, correct_hist_html=correct_hist_html, incorrect_hist_html=incorrect_hist_html)
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+@app.route('/about/')
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
-   app.run()
+    app.run(debug=True)
+
+
---
+++ game_stimuli.py
@@ -0,0 +1,139 @@
+# Pygame Development
+import pygame
+import random
+import time
+
+# Initialize pygame
+pygame.init()
+
+# Constants
+WIDTH, HEIGHT = 800, 600
+CIRCLE_RADIUS = 20
+CIRCLE_COUNT = 100
+SPEED = 5
+
+# Colors
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for _ in range(CIRCLE_COUNT)]
+
+# Create the window
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Circle Collision Game")
+
+class Circle:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.dx = random.choice([-1, 1]) * SPEED
+        self.dy = random.choice([-1, 1]) * SPEED
+
+    def move(self):
+        self.x += self.dx
+        self.y += self.dy
+
+        # Collision with window boundaries
+        if self.x <= CIRCLE_RADIUS or self.x >= WIDTH - CIRCLE_RADIUS:
+            self.dx = -self.dx
+        if self.y <= CIRCLE_RADIUS or self.y >= HEIGHT - CIRCLE_RADIUS:
+            self.dy = -self.dy
+
+    def draw(self, color):
+        pygame.draw.circle(win, color, (self.x, self.y), CIRCLE_RADIUS)
+
+def draw_triangle(center_x, center_y):
+    half_base = 20
+    height = 35
+    point1 = (center_x, center_y + half_base)
+    point2 = (center_x - height, center_y - half_base)
+    point3 = (center_x + height, center_y - half_base)
+    pygame.draw.polygon(win, RED, [point1, point2, point3])
+
+def is_inside_triangle(x, y, center_x, center_y):
+    half_base = 20
+    height = 35
+    return center_x - height <= x <= center_x + height and center_y - half_base <= y <= center_y + half_base
+
+def draw_square(center_x, center_y, size=40):
+    pygame.draw.rect(win, RED, (center_x - size/2, center_y - size/2, size, size))
+
+def is_inside_square(x, y, center_x, center_y, size=40):
+    return center_x - size/2 <= x <= center_x + size/2 and center_y - size/2 <= y <= center_y + size/2
+
+def run_game():
+    circles = [Circle(random.randint(CIRCLE_RADIUS, WIDTH - CIRCLE_RADIUS),
+                      random.randint(CIRCLE_RADIUS, HEIGHT - CIRCLE_RADIUS)) for _ in range(CIRCLE_COUNT)]
+    correct_clicks = 0
+    incorrect_clicks = 0
+
+    correct_latencies = []
+    incorrect_latencies = []
+    shape_displayed = None
+    shape_time = 0
+    shape_center = (0, 0)
+
+    start_time = time.time()
+    run = True
+    while run:
+        if time.time() - start_time > 120:  # 2 minutes duration
+            break
+
+        # Randomly decide to display shape
+        if shape_displayed is None and random.random() < 0.01:
+            shape_displayed = random.choice(["triangle", "square"])
+            shape_time = time.time()
+            shape_center = (random.randint(40, WIDTH - 40), random.randint(40, HEIGHT - 40))
+        # Hide shape after 3 seconds
+        if shape_displayed and time.time() - shape_time > 3:
+            shape_displayed = None
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                if shape_displayed == "triangle" and is_inside_triangle(x, y, *shape_center):
+                    correct_clicks += 1
+                    correct_latencies.append(time.time() - shape_time)
+                    shape_displayed = None
+                if shape_displayed == "triangle" and is_inside_triangle(x, y, *shape_center):
+                    correct_clicks += 1
+                    latencies.append(time.time() - shape_time)
+                    shape_displayed = None
+                elif shape_displayed == "square" and is_inside_square(x, y, *shape_center):
+                    incorrect_clicks += 1
+                    incorrect_latencies.append(time.time() - shape_time)
+                    shape_displayed = None
+                else:
+                    incorrect_clicks += 1
+                    incorrect_latencies.append(time.time() - shape_time)
+
+        win.fill(WHITE)
+
+        # Move and draw circles
+        for i, circle in enumerate(circles):
+            circle.move()
+            circle.draw(colors[i])
+
+        if shape_displayed == "triangle":
+            draw_triangle(*shape_center)
+        elif shape_displayed == "square":
+            draw_square(*shape_center)
+
+        pygame.display.flip()
+        pygame.time.Clock().tick(60)
+
+    pygame.quit()
+    return {
+            "correct_clicks": correct_clicks,
+            "incorrect_clicks": incorrect_clicks,
+            "correct_latencies": correct_latencies,
+            "incorrect_latencies": incorrect_latencies
+            }
+
+if __name__ == "__main__":
+    results = run_game()
+
+    # Save results to a file (or any other storage mechanism)
+    with open("game_results.txt", "w") as f:
+        f.write(str(results))
